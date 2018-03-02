4c76f679b6d5bf5f5755751a7f3a34a386098b85
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-03-02 11:47:03 +1000

    start.sh: fail if conda env is inactive

--- start.sh
+++ start.sh
@@ -5,14 +5,26 @@ declare -r SCRIPT_PATH=$(readlink -f "$0")
 declare -r SCRIPT_DIR=$(cd $(dirname "$SCRIPT_PATH") && pwd)
 
 
+active_conda_env() {
+  conda env list | grep ' \* ' | cut -f 1 -d ' '
+}
+
+
 main() {
   cd $(git rev-parse --show-toplevel)
-  source activate notes
+
+  # source activate notes
+  # calling source activate doesn't work ... so make sure user has done it
+  [[ $(active_conda_env) != "notes" ]] && {
+    echo "ERROR: 'notes' not active; did you forget to 'source activate notes'?"
+    return 1
+  }
 
   export GOPATH=$PWD/go
   export GOBIN=$GOPATH/bin
   export PATH=$PATH:$GOBIN
 
+  set -x
   exec jupyter notebook "$@"
 }
 

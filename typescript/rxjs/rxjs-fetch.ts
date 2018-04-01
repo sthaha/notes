4959ebf5c5756d691d115d837c3e0f62f02aa144
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-04-01 22:36:43 +1000

    rxjs: fake fetch api

--- typescript/rxjs/rxjs-fetch.ts
+++ typescript/rxjs/rxjs-fetch.ts
@@ -1,41 +1,44 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/from';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/merge';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 
-// const rs = Observable.of('https://api.github.com/users');
-
-// rs.flatMap(url => Observable.from(fetch(url)))
-  // .flatMap(res => res.json())
-  // .subscribe(res => console.log(res))
-//
-const stream = Observable.from([1, 2, 3])
-// stream.subscribe(x => console.log('...', x))
+const rs = Observable.of('https://api.github.com/users');
 
 
-// create an observable that emits at regular interval
-//
-
-function counter(name: string,  end: number, timeout: number) {
-  return function(o: any) {
-    let count = 0
-    const interval = setInterval(() => {
-      count++
-      if (count < end) {
-        o.next(`${name}: ${count}`)
-        return
-      }
-
-      // terminate
-      o.complete(count)
-      clearInterval(interval)
-    }, timeout)
-  }
+interface FakeResponse {
+  json(): string
 }
-const first = Observable.create(counter("first",  5, 1000))
-const second = Observable.create(counter("second", 10, 500))
-first.merge(second).subscribe((x: any) => console.log(' ...e: ', x))
+
+const delayed = (timeout: number, val: any): Promise<FakeResponse> => new Promise((accept) => {
+  setTimeout(() => accept(val), timeout)
+})
+
+// delayed(1000, 'works').then(console.log)
+
+
+const fetch = (url: any) => delayed(800, {
+  json: () => [
+    {login: '1'},
+    {login: '2'},
+    {login: '3'},
+  ]
+})
+
+// fetch('foobar').then(x => console.log(x.json()))
+
+rs.flatMap(url => Observable.from(fetch(url)))
+  .flatMap(res => res.json())
+  .flatMap(users => Observable.of(users))
+  // .flatMap(x => {
+    // // console.log("........................................" );
+    // // console.log(x);
+    // // console.log("-----------------------------------------");
+    // return x
+  // })
+  .subscribe((x: any) => console.log(x.login))

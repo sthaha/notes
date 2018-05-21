d482ad7353ae3c988ef4443227658c9322876f73
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-05-21 11:43:58 +1000

    Service: ensure response is set before write

--- go/src/lang/rev-proxy/service.go
+++ go/src/lang/rev-proxy/service.go
@@ -7,13 +7,15 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Service: Got: %s\n", r.URL)
 	response := fmt.Sprintf("Server: Url: %q\n", r.URL)
-	io.WriteString(w, response)
+	time.Sleep(5 * time.Second)
 	w.WriteHeader(200)
+	io.WriteString(w, response)
 }
 
 func startService(ctx context.Context, wg *sync.WaitGroup, addr string, basePath string) {

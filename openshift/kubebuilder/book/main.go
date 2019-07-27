00b4fbf2647fbf49395250cfbff5843130b2de46
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-07-28 00:12:23 +1000

    kubebuilder create api --group batch --version v1 --kind CronJob

--- openshift/kubebuilder/book/main.go
+++ openshift/kubebuilder/book/main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	batchv1 "github.com/sthaha/notes/api/v1"
+	"github.com/sthaha/notes/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = batchv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +65,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.CronJobReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("CronJob"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")

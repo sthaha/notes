3cf6c99ed65ada93f9438bdab3388db9718f6e19
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-09-25 10:54:41 +1000

    Lex: use individual test for lexer

--- compilers/interpreter/lexer/lex_test.go
+++ compilers/interpreter/lexer/lex_test.go
@@ -6,32 +6,44 @@ import (
 	"github.com/sthaha/interpreter/token"
 )
 
-func TestTokenizer(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected []token.Token
-	}{{
-		input: `=+(){}`,
-		expected: []token.Token{
-			tkn(token.Eq, '='),
-			tkn(token.Plus, '+'),
-			tkn(token.LParen, '('),
-			tkn(token.RParen, ')'),
-			tkn(token.LBrace, '{'),
-			tkn(token.RBrace, '}'),
-		},
-	}}
-	for _, tt := range tests {
-		tokenizer := newTokenizer(tt.input)
+func TestTokenizer_Simple(t *testing.T) {
+	input := `=+(){}`
+	expected := []token.Token{
+		tkn(token.Eq, '='),
+		tkn(token.Plus, '+'),
+		tkn(token.LParen, '('),
+		tkn(token.RParen, ')'),
+		tkn(token.LBrace, '{'),
+		tkn(token.RBrace, '}'),
+	}
+	assertTokens(t, input, expected)
+}
 
-		for _, expected := range tt.expected {
-			actual := tokenizer.Next()
-			if actual.Type != expected.Type {
-				t.Fatalf("type error expected: %s | got: %s", expected.Type, actual.Type)
-			}
-			if actual.Value != expected.Value {
-				t.Fatalf("value error expected: %q | got: %q", expected.Value, actual.Value)
-			}
+func assertTokens(t *testing.T, input string, expected []token.Token) {
+	tokenizer := newTokenizer(input)
+
+	for _, expected := range expected {
+		actual := tokenizer.Next()
+		if actual.Type != expected.Type {
+			t.Fatalf("type error expected: %s | got: %s", expected.Type, actual.Type)
+		}
+		if actual.Value != expected.Value {
+			t.Fatalf("value error expected: %q | got: %q", expected.Value, actual.Value)
 		}
 	}
+
 }
+
+//
+//
+// “input := `let five = 5;
+// let ten = 10;
+//
+// let add = fn(x, y) {
+//   x + y;
+// };
+//
+// let result = add(five, ten);
+// `
+// ”
+//

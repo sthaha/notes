20622ebb0809267b7d561494973496d2c4872ef2
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-09-25 21:11:30 +1000

    Lex: identifies let and indentifiers

--- compilers/interpreter/lexer/lex.go
+++ compilers/interpreter/lexer/lex.go
@@ -48,9 +48,17 @@ var (
 	eof       = sym(token.EOF, 0)
 )
 
+func (t *tokenizer) nextChar() (byte, bool) {
+	ch, done := t.read()
+	for isWhitespace(ch) {
+		ch, done = t.read()
+	}
+	return ch, done
+}
+
 func (t *tokenizer) Next() token.Token {
 
-	ch, done := t.read()
+	ch, done := t.nextChar()
 	if done {
 		return eof
 	}
@@ -69,8 +77,57 @@ func (t *tokenizer) Next() token.Token {
 	case '}':
 		return rBrace
 	default:
-		return tkn(token.Illegal)
+
+		if isLetter(ch) {
+			ret := token.Token{}
+			ret.Value = t.readWord()
+			ret.Type = lookupType(ret.Value)
+			// log.Printf("looking up: %q -> %v \n", ret.Value, ret.Type)
+			// log.Printf("returning : %v  \n", ret)
+			return ret
+		}
+		// if isDigit(ch) {
+
+		// }
+		return sym(token.Illegal, ch)
 	}
+}
+
+func (t *tokenizer) readWord() string {
+	prev := t.next - 1
+	for {
+		ch, done := t.read()
+		if done || !isLetter(ch) {
+			break
+		}
+	}
+	return t.input[prev : t.next-1]
+}
+
+var (
+	keywords = map[string]token.Type{
+		"let": token.Let,
+		"fn":  token.Function,
+	}
+)
+
+func lookupType(x string) token.Type {
+	if t, ok := keywords[x]; ok {
+		return t
+	}
+	return token.Identifier
+}
+
+func isLetter(b byte) bool {
+	return ('a' <= b && b <= 'z') || ('A' <= b && b <= 'Z')
+}
+
+func isWhitespace(b byte) bool {
+	switch b {
+	case ' ', '\t', '\n', '\r':
+		return true
+	}
+	return false
 }
 
 func (t *tokenizer) read() (byte, bool) {
--- compilers/interpreter/lexer/lex_test.go
+++ compilers/interpreter/lexer/lex_test.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"log"
 	"testing"
 
 	"github.com/sthaha/interpreter/token"
@@ -8,15 +9,45 @@ import (
 
 func TestTokenizer_Simple(t *testing.T) {
 	input := `=+(){}`
-	expected := []token.Token{
-		tkn(token.Eq, '='),
-		tkn(token.Plus, '+'),
-		tkn(token.LParen, '('),
-		tkn(token.RParen, ')'),
-		tkn(token.LBrace, '{'),
-		tkn(token.RBrace, '}'),
-	}
+	expected := []token.Token{assign, plus, lParen, rParen, lBrace, rBrace, eof}
 	assertTokens(t, input, expected)
+}
+func TestTokenizer_id(t *testing.T) {
+	// let, id("five"), assign, integer("5"), semicolon
+	assertToken(t, token.Token{Type: token.Identifier, Value: "five"}, id("five"))
+}
+
+func TestTokenizer_Helloworld(t *testing.T) {
+
+	input := `
+    let five = 5;
+    let ten = 10;
+
+    let add = fn(x, y) {
+      x + y;
+    };
+
+    let result = add(five, ten);
+	`
+	expected := []token.Token{
+		let, id("five"), assign, integer("5"), semicolon,
+		let, id("ten"), assign, integer("10"), semicolon,
+
+		let, id("add"), assign, fn, lParen, id("x"), coma, id("y"), rParen, lBrace,
+		id("x"), plus, id("y"),
+		rBrace, semicolon,
+	}
+
+	assertTokens(t, input, expected)
+
+}
+func assertToken(t *testing.T, expected, actual token.Token) {
+	if actual.Type != expected.Type {
+		t.Fatalf("type error expected: %s | got: %s", expected.Type, actual.Type)
+	}
+	if actual.Value != expected.Value {
+		t.Fatalf("value error expected: %q | got: %q", expected.Value, actual.Value)
+	}
 }
 
 func assertTokens(t *testing.T, input string, expected []token.Token) {
@@ -24,26 +55,8 @@ func assertTokens(t *testing.T, input string, expected []token.Token) {
 
 	for _, expected := range expected {
 		actual := tokenizer.Next()
-		if actual.Type != expected.Type {
-			t.Fatalf("type error expected: %s | got: %s", expected.Type, actual.Type)
-		}
-		if actual.Value != expected.Value {
-			t.Fatalf("value error expected: %q | got: %q", expected.Value, actual.Value)
-		}
+		log.Printf("   ... %v   |   %v", actual, expected)
+		assertToken(t, expected, actual)
 	}
 
 }
-
-//
-//
-// “input := `let five = 5;
-// let ten = 10;
-//
-// let add = fn(x, y) {
-//   x + y;
-// };
-//
-// let result = add(five, ten);
-// `
-// ”
-//
